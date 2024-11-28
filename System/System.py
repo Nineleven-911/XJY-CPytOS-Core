e66a708330c201efa010d69204b840645222a0b7
Nineleven-911 <mc.20121112.bd@outlook.com>
2024-11-28 22:10:47 +0800

    Needed Updates...

--- System/System.py
+++ System/System.py
@@ -3,6 +3,8 @@ import os, shutil
 """Defined Function Program Version 0.01.0"""
 """Public:Variables & Consts"""
 const_SystemRootDirectory = os.getcwd()
+const_LoginUserDirectory = os.getcwd()
+const_loginUser = ""
 
 
 #def setColor(color):
@@ -10,20 +12,24 @@ const_SystemRootDirectory = os.getcwd()
 
 def version():
     print(
-        "XJY-DOS-Base Disk Operating System(Console Version)\nCopyright (C)2023-2024 JinY Corporation.All rights reserved.\nVersions:DOS Alpha 0.01.0 | Visual C++ 2024 | Python CPython 3.12.5 x64\n! Internal Test Version !")
+        """XJY-DOS-Base Disk Operating System(Console Version)
+Copyright (C)2023-2024 JinY Corporation.All rights reserved.\nVersions:DOS Alpha 0.01.1.24001 | Visual C++ 2024 | Python CPython 3.12.5 x64
+! Internal Test Version !""")
 
 
-"""def judgeCommand(inputcommand:str):
-    # Class Def Variables
+def judgeCommand(inputcommand: str):
+    """Class Def Variables"""
     fileCmd = file()
-    """"""
+    userCmd = user()
+    """-----Class End-----"""
     command = ""
     counter = 0
     parameter = []
+    parameterWithMark = []
     for _ in inputcommand:
         if _ == "(":
             command = inputcommand[:counter]
-            parameterWithMark = list(map(str,inputcommand[counter+1:-1].split(",")))
+            parameterWithMark = list(map(str, inputcommand[counter + 1:-1].split(",")))
             break
         counter += 1
     for _ in parameterWithMark:
@@ -38,63 +44,28 @@ def version():
             raise NameError("There's no Function called \"" + inputcommand + "\".")
     elif command[:5] == "file.":
         classway = command[5:]
-        if classway == "recoveryDefaultDir":
-            file.recoveryDefaultDir()
+        if classway == "recoverDefaultDir":
+            file.recoverDefaultDir()
         elif classway == "createFolder":
-            file.createFolder(fileCmd,parameter[0],parameter[1])
+            file.createFolder(fileCmd, parameter[0], parameter[1])
         elif classway == "deleteFolder":
-            file.deleteFolder(fileCmd,parameter[0],parameter[1])
+            file.deleteFolder(fileCmd, parameter[0], parameter[1])
         elif classway == "createFile":
-            file.createFile(fileCmd,parameter[0],parameter[1])
+            file.createFile(fileCmd, parameter[0], parameter[1])
         elif classway == "deleteFile":
-            file.deleteFile(fileCmd,parameter[0],parameter[1])
+            file.deleteFile(fileCmd, parameter[0], parameter[1])
         elif classway == "writeFileText":
-            file.writeFileText(fileCmd,parameter[0],parameter[1],parameter[2])
+            file.writeFileText(fileCmd, parameter[0], parameter[1], parameter[2])
         else:
             raise NameError("There's no Function called \"" + inputcommand + "\".")
+    elif command[:5] == "user.":
+        classway = command[5:]
+        if classway == "addUser":
+            user.addUser(userCmd, parameter[0], parameter[1])
     elif command == "version":
         version()
     else:
-        raise NameError("There's no Function called \""+inputcommand+"\".")"""
-
-
-def judgeCommand(inputcommand: str):
-    """Class Def Variables"""
-    fileCmd = file()
-    """"""
-    command = ""
-    counter = 0
-    parameter = []
-    for _ in inputcommand:
-        if _ == "(":
-            command = inputcommand[:counter]
-            parameterWithMark = list(map(str, inputcommand[counter + 1:-1].split(",")))
-            break
-        counter += 1
-    for _ in parameterWithMark:
-        parameter.append(_[1:-1])
-
-    match command.split("."):
-        case ["check", "IOCheck"]:
-            check.IOCheck()
-        case ["check", "ConsoleCheck"]:
-            check.ConsoleCheck()
-        case ["file", "recoveryDefaultDir"]:
-            file.recoveryDefaultDir()
-        case ["file", "createFolder"]:
-            file.createFolder(fileCmd, parameter[0], parameter[1])
-        case ["file", "deleteFolder"]:
-            file.deleteFolder(fileCmd, parameter[0], parameter[1])
-        case ["file", "createFile"]:
-            file.createFile(fileCmd, parameter[0], parameter[1])
-        case ["file", "deleteFile"]:
-            file.deleteFile(fileCmd, parameter[0], parameter[1])
-        case ["file", "writeFileText"]:
-            file.writeFileText(fileCmd, parameter[0], parameter[1], parameter[2])
-        case ["version"]:
-            version()
-        case _:
-            raise NameError("There's no Function called \"" + inputcommand + "\".")
+        raise NameError("There's no Function called \"" + inputcommand + "\".")
 
 
 # 这个函数只对未编译的Python源文件使用。
@@ -123,21 +94,44 @@ def _readUnicodeString(unicodestring):
     return outstr
 
 
-def start():
-    print("[DOS]Starting System...")
+def _start():
+    a = ""
+    print("[POS]Starting System...")
     with open(const_SystemRootDirectory + "\\System\\SAM", "r+") as f:
         f.seek(0)
-        return _readUnicodeString(f.read())
+        a = f.read()
+    return _userJudge(a)
+
+
+def _userJudge(st):
+    statements = list(_readUnicodeString(st).split(" "))
+    userDat = {}
+    tmp = 0;
+    usernum = 0
+    for i in statements:
+        # Judge Username and Userpwd
+        if "MainUser:" == i[:9]:
+            # Main User is equal to User0
+            userDat[i[9:]] = statements[tmp + 1]
+            usernum += 1
+        if i[:5] == "User:":
+            userDat[i[5:]] = statements[tmp + 1]
+        tmp += 1
+    return userDat
 
 
 def _analysisPaths(path):
     """C:==System\\"""
-    outstr = ""
-    outstr = os.getcwd() + "\\System\\" + path[3:]
+    outstr = const_LoginUserDirectory + path[3:]
     return outstr
 
 
+def _nowDirectory():
+    return const_LoginUserDirectory
+
+
 class check:
+    @staticmethod
     def IOCheck():
         os.mkdir("checkFolder")
         f = open("checkFolder\\checkUACFile.log", "w+")
@@ -149,16 +143,16 @@ class check:
         os.remove("checkFolder\\checkUACFile.log")
         os.rmdir("checkFolder")
         if _readUnicodeString(checkText) == "checking...AaBbCcDdEeFfGgHhIiGjKk--UTF-8!@#$%^&*\\\"\'":
-            print("[System\\definedFunction]<check>IOcheck:IOcheck Success.")
+            print("[System]<check>IOcheck:IOcheck Success.")
             return True
         else:
-            print("[System\\definedFunction]<check>IOcheck:IOcheck Failed!")
+            print("[System]<check>IOcheck:IOcheck Failed!")
             return False
 
     def ConsoleCheck():
         print(
             "ABCDEFGHIGKLMNOPQRSTUVWXYZabcdefghijklmnoptrstuvwxyz-Backspace\" \"-~!@#$%^&*()`_+-=1234567890[]\\;\',./{}|:\"<>?\'")
-        print("[System\\definedFunction]<check>Consolecheck:ConsoleCheck Run Successful.")
+        print("[System]<check>Consolecheck:ConsoleCheck Run Successful.")
 
 
 class file:
@@ -186,7 +180,7 @@ class file:
         print("[FileSystem]Folder Deleted.")
 
     def createFile(self, path, filename):
-        f = open(_analysisPaths(path) + filename, "w+")
+        f = open(_analysisPaths(path) + "\\" + filename, "w+")
         f.close()
         print("[FileSystem]File created.")
 
@@ -195,12 +189,23 @@ class file:
         file.recoverDefaultDir()
         print("[FileSystem]File Deleted.")
 
-    def writeFileText(self, path, file, text, mode):
+    def writeFileText(self, path, text, mode):
         path = _analysisPaths(path)
         if mode == "add":
-            with open(path + "\\" + file, "a") as f:
+            with open(path, "a") as f:
                 f.write(text)
         elif mode == "fill":
-            with open(path + "\\" + file, "w") as f:
+            with open(path, "w") as f:
                 f.write(text)
         print("[FileSystem]File text wrote.")
+
+
+
+class user:
+    def addUser(self, username, password=""):
+        with open(const_SystemRootDirectory + "\\System\\SAM", "a") as f:
+            f.write(" " + _writeUnicodeString("User:" + username + " " + password))
+
+    def deleteUser(self, username):
+        with open(const_SystemRootDirectory + "\\System\\SAM", "w") as f:
+            pass
