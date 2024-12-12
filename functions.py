c75cb596de51f11950c489e90146e67ca63eabfa
Nineleven-911 <mc.20121112.bd@outlook.com>
2024-12-12 22:59:42 +0800

    Debug Update

--- functions.py
+++ functions.py
@@ -1,29 +1,36 @@
-# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*- 
 
 import os
 
 import system
 
 
-class user:
+class user:  # 账号类
     @staticmethod
     def login(usr):
-        print("Login: \nGuest account name: guest, password: PWD\nAccounts:")
-        for i in usr.items():
-            print(i[0], end=" ")
-        while 1:
-            acc_name = input("\nAccount name: ")
-            if acc_name not in usr.keys():
-                system.info.warning_info("Invalid account name.")
-                system.write_log(f"Login failed. Input account name: \"{acc_name}\"")
-                continue
-            pwd = input("Password:")
-            if usr[acc_name][0] != pwd:
-                system.info.warning_info("Invalid account password, password:", pwd)
-                system.write_log(f"Login failed. Input password: \"{pwd}\"")
-                continue
-            os.system("cls" if os.name == "nt" else "clear")
-            system.info.normal_info(system.consts.OS_ICON, "\n" + "-" * 30 + "\n")
-            system.info.normal_info("Welcome!", acc_name)
-            system.write_log(f"Login success. Login account: \"{acc_name}\"")
-            return acc_name
+        if usr == {}:  # 判断是否有账号
+            system.info.warning_info("No account.")  # 输出警告信息
+            system.write_log("Login failed. No account.")  # 输出日志
+            return "Guest"  # 返回账号名称  # 返回账号名称
+        # 登录
+        print("Login: \nGuest account name: Guest, password: 1234\nAccounts:")  # 输出所有账号
+        for i in usr.items():  # 输出所有账号 
+            print(i[0], end=" ")  # 输出账号名
+        print()  # 换行
+        while 1:  # 循环
+            acc_name = input("\nAccount name: ")  # 输入账号名
+            if acc_name not in usr.keys():  # 判断账号名是否存在
+                system.info.warning_info("Invalid account name.")  # 输出警告信息
+                system.write_log(f"Login failed. Input account name: \"{acc_name}\"")  # 输出日志
+                continue  # 继续循环
+            pwd = input("Password:")  # 输入密码
+            if usr[acc_name][0] != pwd:  # 判断密码是否正确
+                system.info.warning_info("Invalid account password, password:", pwd)  # 输出警告信息
+                system.write_log(f"Login failed. Input password: \"{pwd}\"")  # 输出日志
+                continue  # 继续循环
+            # 登录成功,返回账号名
+            os.system("cls" if os.name == "nt" else "clear")  # 清屏
+            system.info.normal_info(system.consts.OS_ICON, "\n" + "-" * 30 + "\n")  # 输出提示信息
+            system.info.normal_info("Welcome!", acc_name)  # 输出提示信息
+            system.write_log(f"Login success. Login account: \"{acc_name}\"")  # 输出日志
+            return acc_name  # 返回账号名
--- main.py
+++ main.py
@@ -25,25 +25,23 @@ is_login = False
 # Initiation for Nuitka (为Nuitka的初始化，因为Nuitka的处理逻辑是: 不用到，不打包)
 colorama.init()
 os.system("cls" if os.name == "nt" else "clear")
-usr = system.read_config()
+usr = system.read_users()
 print(
     fr"""XJY-DOS <Alpha 0.02.0 Remake>
 [Development Version]: Build in XJY-CPython Operating System Core Version <0.01.0>
 {system.consts.OS_ICON}
 """)
-system.info.normal_info("[OS]: Need login(Catch exception) - [xjy.os.status.exception.NeedLoginException]")
 
 # Initiation ends
 
 # Login 登录账户部分
-
 account_logged_in = func.user.login(usr)
 
 # Main Loop 主循环
 command_case = ""
 command_args = []
 while 1:
-    try:
+    try:  # 循环主体
         command = input(" >>>")
         # Split command
         command_case = "NaS"
@@ -53,19 +51,24 @@ while 1:
         else:
             command_case = command[:command.find("(")]
             command_args: list = command[command.find("(") + 1:command.rfind(")")].split(",")
+        if command_case == "":
+            continue
 
         # Judge 判断指令
 
-        """
-        # It will be executed if there's a prefix in command. 当命令有前缀时, 激活此处判断语句
+        """ # Prefix Commands 前缀命令
+        # It will be executed if there's a prefix in command. 当命令有前缀时, 激活此处判断语句 
         if "." in command_case:
             command_without_prefix = command_case[command.find(".") + 1:]
             if command[:command.find(".")] == "os":
                 match command_without_prefix:
                     case "version":
-                    pass
+                        pass
 
-            continue"""
+            """
+
+        # Test Commands 测试命令
+        # print(system.write_user_log("Admin1", "123456", "admin", usr))
 
         match command_case:
             # OS Basic Commands 基础命令(操作系统底层命令)
@@ -81,13 +84,110 @@ XJY-CPython Operating System Core <0.01.0> \nOS Version: Alpha Build 00101.03067
             case "cls":
                 os.system("cls" if os.name == "nt" else "clear")
 
+            case "debugger":  # 调试器
+                if usr[account_logged_in][1] != "admin":
+                    raise PermissionError("Have no enough permissions")
+                os.system("cls" if os.name == "nt" else "clear")
+                print(system.consts.OS_ICON + "\n" +
+                      system.consts.OS_DEBUGGER_ICON
+                      )
+                login_success: bool = False
+                scope = system.CommandScope()
+                user_pwd = ""
+                while not login_success:  # 终端用户验证
+                    user_pwd = input("PWD >>>")
+                    if user_pwd == "1111":
+                        login_success = True
+                        break
+                    if user_pwd == "":
+                        break
+                else:
+                    while 1:
+                        scope.command = input(" -").lower()
+                        scope.command_case = scope.command.split(" ")[0]
+                        scope.args = [value for value in scope.command.split(" ")[1:] if value != ""]
+                        # Debug
+                        # print(scope.command, " | ", scope.command_case, " | ", scope.args)
+                        try:
+                            match scope.command_case.lower():
+                                case "exit":
+                                    break
+                                case "":
+                                    continue
+                                case "cls":
+                                    os.system("cls" if os.name == "nt" else "clear")
+                                case "db":
+                                    if len(scope.args) == 0:
+                                        print("No arguments.")
+                                        continue
+                                    if (scope.args[0] == "--check" and not system.chars_in(
+                                                scope.args[1], ["(", ")", ",", ".", "|", "?", "!", "~"]
+                                            )):
+                                        print(eval(scope.args[1]))
+                                    else:
+                                        print("Invalid argument.")
+                                case _:
+                                    print("Invalid command.")
+                        except EOFError:
+                            print("EOF Exited.")
+                            break
+                        except Exception as err:
+                            system.info.error_info(str(err))
+                os.system("cls" if os.name == "nt" else "clear")
+                print(system.consts.OS_ICON)
+
             # User Commands 用户命令
             case "relogin":
                 system.write_log("Login status be turned to: non_login")
                 account_logged_in = func.user.login(usr)
 
             case "logining":
-                system.info.normal_info(account_logged_in)
+                system.info.normal_info(
+                    "User name: " + account_logged_in)
+
+            case "adduser":
+                system.require_args(3, command_args)
+                if usr[account_logged_in][1] != "admin":
+                    raise PermissionError("Have no enough permissions")
+                system.write_users(
+                    system.encrypt(system.write_user_log(command_args[0], command_args[1], command_args[2], usr))
+                )
+                system.write_log(f"Use command: \"{command_case}\", args are not viewable.")
+                continue
+
+            case "deluser":
+                system.require_args(1, command_args)
+                if usr[account_logged_in][1] != "admin":
+                    raise PermissionError("Have no enough permissions")
+                result = ""
+                for key, value in usr.items():
+                    if key != command_args[0]:
+                        result += f"{key}:{value[0]}?{value[1]},"
+                system.write_users(
+                    system.encrypt(result[:-1])
+                )
+                system.write_log(f"Use command: \"{command_case}\", args are not viewable.")
+                continue
+
+            case "chgpwd":
+                system.require_args(2, command_args)
+                if usr[account_logged_in][1] != "admin":
+                    raise PermissionError("Have no enough permissions")
+                result = ""
+                found = False
+                for key, value in usr.items():
+                    if key != command_args[0]:
+                        result += f"{key}:{value[0]}?{value[1]},"
+                    else:
+                        result += f"{key}:{command_args[1]}?{value[1]},"
+                        found = True
+                if not found:
+                    system.info.normal_info(f"There is no user called \"{command_args[0]}\"")
+                system.write_users(
+                    system.encrypt(result[:-1])
+                )
+                system.write_log(f"Use command: \"{command_case}\", args are not viewable.")
+                continue
 
             # Applications Commands 软件命令
             case "apps":
@@ -102,12 +202,12 @@ XJY-CPython Operating System Core <0.01.0> \nOS Version: Alpha Build 00101.03067
                 app_name = command_args[0]
                 if app_name not in os.listdir(os.getcwd() + "\\Program Files (p3.12)\\"):
                     raise FileNotFoundError(f"Can't find an application called \"{app_name}\"")
-                elif account_logged_in == "guest":
+                elif usr[account_logged_in][1] == "guest":
                     raise PermissionError("Have no enough permissions")
                 os.system(f"python \"{os.getcwd() + "\\Program Files (p3.12)\\" + app_name}\"")
 
             # IO Commands I/O 命令
-            case "text":
+            case "type":
                 system.require_args(1, command_args)
                 file_loc = os.getcwd() + "\\" + command_args[0]
                 with open(file_loc, encoding="UTF-8") as f:
@@ -147,11 +247,11 @@ XJY-CPython Operating System Core <0.01.0> \nOS Version: Alpha Build 00101.03067
 
             case "rm":
                 system.require_args(1, command_args)
-                os.remove(os.getcwd()+"\\"+command_args[0])
+                os.remove(os.getcwd() + "\\" + command_args[0])
 
             # Special Commands 特殊命令
             case "is_admin":
-                if account_logged_in == "guest":
+                if usr[account_logged_in][1] == "guest":
                     raise PermissionError("Have no enough permissions")
                 system.info.normal_info("You are administrator!")
 
@@ -164,7 +264,9 @@ Not a Statement)'"}\"")
 
         system.write_log(f"Use command: \"{command_case}\", args: {command_args}")
 
+    except EOFError:
+        print("EOF Exited.")
     except Exception as error:
         system.write_log(f"Use command failed: \"{command_case}\", args: {command_args}, cause \
-by \"{error}\"")
+by \"{str(error)}\"")
         system.info.error_info("Catch Runtime Error:", str(error))
